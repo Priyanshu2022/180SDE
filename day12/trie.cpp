cbd3e189dd1e3233c51df5354c77c265fcfe6080
unknown <priyanshupatwa45@gmail.com>
2022-05-17 21:49:23 +0530

    day12

---
+++ day12/trie.cpp
@@ -0,0 +1,178 @@
+// trie 1
+// insert
+// search
+// startsWith
+// links[26] and flag
+struct Node{
+    Node* links[26];
+    bool flag=false;
+    bool containsKey(char ch){
+        return (links[ch-'a']!=NULL);
+    }
+    void put(char ch,Node* node){
+        links[ch-'a']=node;
+    }
+    Node* get(char ch){
+        return links[ch-'a'];
+    }
+    void setEnd(){
+        flag=true;
+    }
+    bool isEnd(){
+        return flag;
+    }
+};
+class Trie {
+    Node* root;
+public:
+    Trie() {
+        root=new Node();
+    }
+    
+    void insert(string word) {
+        Node* node=root;
+        for(int i=0;i<word.length();i++){
+            if(!node->containsKey(word[i])){
+                node->put(word[i],new Node());
+            }
+            node=node->get(word[i]);
+        }
+        node->setEnd();
+    }
+    
+    bool search(string word) {
+        Node* node=root;
+        for(int i=0;i<word.length();i++){
+            if(!node->containsKey(word[i])){
+                return false;
+            }
+            node=node->get(word[i]);
+        }
+         return node->isEnd();
+    }
+    
+    bool startsWith(string prefix) {
+        Node* node=root;
+        for(int i=0;i<prefix.length();i++){
+            if(!node->containsKey(prefix[i])){
+                return false;
+            }
+            node=node->get(prefix[i]);
+        }
+        return true;
+    }
+};
+
+// trie 2
+// insert
+// count words equal to
+// count words starting with
+// erase
+// links[26],cw(count words) ,cp(count prefix)
+struct Node {
+  Node * links[26];
+  int cntEndWith = 0;
+  int cntPrefix = 0;
+
+  bool containsKey(char ch) {
+    return (links[ch - 'a'] != NULL);
+  }
+  Node * get(char ch) {
+    return links[ch - 'a'];
+  }
+  void put(char ch, Node * node) {
+    links[ch - 'a'] = node;
+  }
+  void increaseEnd() {
+    cntEndWith++;
+  }
+  void increasePrefix() {
+    cntPrefix++;
+  }
+  void deleteEnd() {
+    cntEndWith--;
+  }
+  void reducePrefix() {
+    cntPrefix--;
+  }
+  int getEnd() {
+    return cntEndWith;
+  }
+  int getPrefix() {
+    return cntPrefix;
+  }
+};
+class Trie {
+  private:
+    Node * root;
+
+  public:
+    /** Initialize your data structure here. */
+    Trie() {
+      root = new Node();
+    }
+
+  /** Inserts a word into the trie. */
+  void insert(string word) {
+    Node * node = root;
+    for (int i = 0; i < word.length(); i++) {
+      if (!node -> containsKey(word[i])) {
+        node -> put(word[i], new Node());
+      }
+      node = node -> get(word[i]);
+      node -> increasePrefix();
+    }
+    node -> increaseEnd();
+  }
+
+ int countWordsEqualTo(string &word)
+    {
+        Node *node = root;
+        for (int i = 0; i < word.length(); i++)
+        {
+            if (node->containsKey(word[i]))
+            {
+                node = node->get(word[i]);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        return node->getEnd();
+    }
+
+
+  int countWordsStartingWith(string & word) {
+    Node * node = root;
+    for (int i = 0; i < word.length(); i++) {
+      if (node -> containsKey(word[i])) {
+        node = node -> get(word[i]);
+      } else {
+        return 0;
+      }
+    }
+    return node -> getPrefix();
+  }
+
+  void erase(string & word) {
+    Node * node = root;
+    for (int i = 0; i < word.length(); i++) {
+      if (node -> containsKey(word[i])) {
+        node = node -> get(word[i]);
+        node -> reducePrefix();
+      } else {
+        return;
+      }
+    }
+    node -> deleteEnd();
+  }
+};
+
+// Longest word with all prefix
+// given vector of strings
+// we have to tell longest string whose every prefix is present
+
+
+// count distinct substrings
+// given a string find number of distinct substrings
